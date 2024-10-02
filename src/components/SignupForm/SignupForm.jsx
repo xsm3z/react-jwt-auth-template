3e5027e945e1b50fc1fa7297ef63f67e632c8a77
xsm3z <xsm.zhang@gmail.com>
2024-10-01 20:43:52 -0400

    signup form created

---
+++ src/components/SignupForm/SignupForm.jsx
@@ -0,0 +1,79 @@
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+
+const SignupForm = (props) => {
+  const navigate = useNavigate();
+  const [message, setMessage] = useState(['']);
+  const [formData, setFormData] = useState({
+    username: '',
+    password: '',
+    passwordConf: '',
+  });
+
+  const updateMessage = (msg) => {
+    setMessage(msg);
+  };
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    updateMessage('');
+    console.log(formData); // this line will print the form data to the console
+  };
+
+  const { username, password, passwordConf } = formData;
+
+  const isFormInvalid = () => {
+    return !(username && password && password === passwordConf);
+  };
+
+  return (
+    <main>
+      <h1>Sign Up</h1>
+      <p>{message}</p>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="username">Username:</label>
+          <input
+            type="text"
+            id="name"
+            value={username}
+            name="username"
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            name="password"
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="confirm">Confirm Password:</label>
+          <input
+            type="password"
+            id="confirm"
+            value={passwordConf}
+            name="passwordConf"
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <button disabled={isFormInvalid()}>Sign Up</button>
+          <Link to="/">
+            <button>Cancel</button>
+          </Link>
+        </div>
+      </form>
+    </main>
+  );
+};
+
+export default SignupForm;
