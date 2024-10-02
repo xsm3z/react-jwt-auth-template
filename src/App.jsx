800851ccbf56e01b656f41358ac129d437353efa
xsm3z <xsm.zhang@gmail.com>
2024-10-01 20:40:35 -0400

    landing, dashboard created. Updated nav and app

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,24 @@
 import { useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import NavBar from './components/NavBar/NavBar';
+import Landing from './components/Landing/Landing';
+import Dashboard from './components/Dashboard/Dashboard';
 
 const App = () => {
-  const [user, setUser] = useState(null)
+  const [user, setUser] = useState(null);
 
   return (
     <>
       <NavBar user={user} />
-      <h1>Hello world!</h1>
+      <Routes>
+        { user ? (
+          <Route path="/" element={<Dashboard user={user} />} />
+        ) : (
+          <Route path="/" element={<Landing />} />
+        )}
+      </Routes>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,8 +1,25 @@
+import { Link } from 'react-router-dom';
+
 const NavBar = ({ user }) => {
   return (
     <>
-      <p>Hello Navbar</p>
+      { user ? (
+        <nav>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="">Sign Out</Link></li>
+          </ul>
+        </nav>
+      ) : (
+        <nav>
+          <ul>
+            <li><Link to="/signin">Sign In</Link></li>
+            <li><Link to="/signup">Sign Up</Link></li>
+          </ul>
+        </nav>
+      )}
     </>
   )
 }
-export default NavBar
+
+export default NavBar;
